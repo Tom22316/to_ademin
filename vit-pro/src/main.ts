fe52ce7e132c1cecd0606325f5d65180d4018208
userName <userEmail>
2024-09-23 14:03:12 +0800

    解决element plus无法使用样式

--- vit-pro/src/main.ts
+++ vit-pro/src/main.ts
@@ -2,8 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus';
 
 // css
 import 'element-plus/dist/index.css';
 
-createApp(App).use(router).mount('#app')
+
+
+const app = createApp(App);
+app.use(router);
+app.use(ElementPlus);
+app.mount('#app');
--- vit-pro/src/views/login/index.vue
+++ vit-pro/src/views/login/index.vue
@@ -1,50 +1,26 @@
 <script lang="ts" setup>
-import { reactive, ref } from 'vue'
-import type { FormInstance } from 'element-plus'
-
-const ruleFormRef = ref<FormInstance>()
-const ruleForm = reactive({
-    pass: '',
-    checkPass: '',
-    age: '',
-})
-
-const submitForm = (formEl: FormInstance | undefined) => {
-    if (!formEl) return
-    formEl.validate((valid) => {
-        if (valid) {
-            console.log('登录成功!')
-        } else {
-            console.log('登录失败!')
-        }
-    })
-}
 
 </script>
 
 <template>
     <div class="login-container">
+        <div class="switch">11</div>
+        <div class="owl"></div>
         <div class="login-card">
+            <div class="title">11</div>
             <div class="content">
-                <el-form ref="ruleFormRef" :model="ruleForm">
-                    <el-form-item label="账号" prop="pass">
-                        <el-input v-model="ruleForm.pass" type="password" autocomplete="off" />
+                <el-form ref="ruleFormRef">
+                    <el-form-item prop="username">
+                        <el-input type="text" placeholder="用户名" size="large" />
                     </el-form-item>
-                    <el-form-item label="密码" prop="age">
-                        <el-input v-model.number="ruleForm.age" />
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary" @click="submitForm(ruleFormRef)">
-                            登录
-                        </el-button>
+                    <el-form-item prop="password">
+                        <el-input placeholder="密码" size="large" show-password />
                     </el-form-item>
                 </el-form>
             </div>
 
         </div>
     </div>
-
-
 </template>
 
 <style lang="scss" scoped>
@@ -57,16 +33,46 @@ const submitForm = (formEl: FormInstance | undefined) => {
     width: 100%;
     min-height: 100%;
 
+    .switch {
+        position: fixed;
+        top: 5%;
+        right: 5%;
+        cursor: pointer;
+    }
+
+    .owl {
+        position: relative;
+        width: 120px;
+        height: 95px;
+        transform: translateY(12%);
+        background-image: url(/src/assets/login/face.png);
+        background-repeat: no-repeat;
+        background-size: 100%;
+    }
+
     .login-card {
         width: 480px;
-        height: 444px;
+        max-width: 90%;
         border-radius: 20px;
         box-shadow: 0 0 10px #dcdfe6;
         background-color: var(--el-bg-color);
         overflow: hidden;
 
+        .title {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 150px;
+        }
+
         .content {
             padding: 20px 50px 50px 50px;
+
+            .el-button {
+                width: 100%;
+                margin-top: 10px;
+            }
+
         }
     }
 }
